e8b41edda7ea520bbc1ef11861959487fa35c644
sumtopmus <sumtopmus@users.noreply.github.com>
2015-06-12 16:12:37 -0400

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Oleksandr Iaroshenko on 12.06.15.
+//  Copyright (c) 2015 Oleksandr Iaroshenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
